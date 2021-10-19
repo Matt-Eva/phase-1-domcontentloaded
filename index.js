cde73b9d4f0caf2b371bbfb31c67c2e1cf144b15
Matt Eva <matteweva@gmail.com>
2021-10-18 17:37:54 -0700

    commit tree c4c0b111313f1cf96105c739a25f3fe0c7b68c32

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('#text').textContent = 'This is really cool!'
+})
+
+document.querySelector('#text').addEventListener('click', function(){
+    document.querySelector('#text').textContent = 'JavaScript is so cool. It lets me add text to my page programmatically.'
+})
+
+console.log(document.querySelector('#text').textContent);
